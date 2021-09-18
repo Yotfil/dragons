f378fda721595ec9f86acf8d4d58514841874c24
Jefry Guevara <jefryeguevarav@gmail.com>
2021-09-17 23:27:50 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,179 @@
+const DataDragons = [
+  {
+    "Nombre": "Dragón Rojo",
+    "Tipo": [
+     "Fuego"
+    ],
+    "Descripcion":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae sapiente ipsum eveniet asperiores impedit, minima labore vitae.",
+    "Ataque":[
+      {
+        "Nombre": "Normal",
+        "Daño":"1000"
+      },
+      {
+        "Nombre": "Especial",
+        "Daño":"2000"
+      }
+    ],
+    "Defensa":"1500",
+    "Vida":"5000"
+  },
+  {
+    "Nombre": "Dragón Verde",
+    "Tipo": [
+     "Ácido"
+    ],
+    "Descripcion":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae sapiente ipsum eveniet asperiores impedit, minima labore vitae.",
+    "Ataque":[
+      {
+        "Nombre": "Normal",
+        "Daño":"900"
+      },
+      {
+        "Nombre": "Especial",
+        "Daño":"2200"
+      }
+    ],
+    "Defensa":"1300",
+    "Vida":"5500"
+  },
+  {
+    "Nombre": "Dragón Amarillo",
+    "Tipo": [
+     "Trueno"
+    ],
+    "Descripcion":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae sapiente ipsum eveniet asperiores impedit, minima labore vitae.",
+    "Ataque":[
+      {
+        "Nombre": "Normal",
+        "Daño":"1100"
+      },
+      {
+        "Nombre": "Especial",
+        "Daño":"1800"
+      }
+    ],
+    "Defensa":"1800",
+    "Vida":"5300"
+  },
+  {
+    "Nombre": "Dragón Azul",
+    "Tipo": [
+     "Agua"
+    ],
+    "Descripcion":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae sapiente ipsum eveniet asperiores impedit, minima labore vitae.",
+    "Ataque":[
+      {
+        "Nombre": "Normal",
+        "Daño":"800"
+      },
+      {
+        "Nombre": "Especial",
+        "Daño":"2500"
+      }
+    ],
+    "Defensa":"1000",
+    "Vida":"6000"
+  },
+  {
+    "Nombre": "Dragón Café",
+    "Tipo": [
+     "Tierra"
+    ],
+    "Descripcion":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae sapiente ipsum eveniet asperiores impedit, minima labore vitae.",
+    "Ataque":[
+      {
+        "Nombre": "Normal",
+        "Daño":"1100"
+      },
+      {
+        "Nombre": "Especial",
+        "Daño":"2200"
+      }
+    ],
+    "Defensa":"1000",
+    "Vida":"4000"
+  },
+  {
+    "Nombre": "Dragón Blanco",
+    "Tipo": [
+     "Aire"
+    ],
+    "Descripcion":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae sapiente ipsum eveniet asperiores impedit, minima labore vitae.",
+    "Ataque":[
+      {
+        "Nombre": "Normal",
+        "Daño":"1000"
+      },
+      {
+        "Nombre": "Especial",
+        "Daño":"1900"
+      }
+    ],
+    "Defensa":"1500",
+    "Vida":"5500"
+  },
+  {
+    "Nombre": "Dragón Blanco de Ojos Azules",
+    "Tipo": [
+     "Agua",
+     "Aire",
+     "Fuego"
+    ],
+    "Descripcion":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae sapiente ipsum eveniet asperiores impedit, minima labore vitae.",
+    "Ataque":[
+      {
+        "Nombre": "Normal",
+        "Daño":"2000"
+      },
+      {
+        "Nombre": "Especial",
+        "Daño":"4000"
+      }
+    ],
+    "Defensa":"2500",
+    "Vida":"9000"
+  },
+  {
+    "Nombre": "Dragón Cobre",
+    "Tipo": [
+     "Tierra",
+     "Fuego"
+    ],
+    "Descripcion":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae sapiente ipsum eveniet asperiores impedit, minima labore vitae.",
+    "Ataque":[
+      {
+        "Nombre": "Normal",
+        "Daño":"2000"
+      },
+      {
+        "Nombre": "Especial",
+        "Daño":"4000"
+      }
+    ],
+    "Defensa":"2500",
+    "Vida":"19000"
+  },
+  {
+    "Nombre": "Dragón Bronce",
+    "Tipo": [
+     "Acido",
+     "Aire",
+    ],
+    "Descripcion":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae sapiente ipsum eveniet asperiores impedit, minima labore vitae.",
+    "Ataque":[
+      {
+        "Nombre": "Normal",
+        "Daño":"2000"
+      },
+      {
+        "Nombre": "Especial",
+        "Daño":"14000"
+      }
+    ],
+    "Defensa":"2500",
+    "Vida":"11000"
+  },
+]
+
+export default DataDragons
---
+++ index.js
@@ -0,0 +1,139 @@
+import DataDragons from './data.js';
+
+
+
+/*
+
+  Arrays a utilizar
+
+  const nombres = ["Paola", "Karina", "Jose", "Daniel", "Luis"]
+
+  const usuarios = [
+    {
+      id: 1,
+      nombre: "Juan",
+      apellido: "Diaz",
+      activo: true
+    },
+    {
+      id: 2,
+      nombre: "Diana",
+      apellido: "Perez",
+      activo: false
+    },
+    {
+      id: 3,
+      nombre: "Paola",
+      apellido: "Acosta",
+      activo: true
+    },
+    {
+      id: 4,
+      nombre: "Carlos",
+      apellido: "Forero",
+      activo: true
+    },
+    {
+      id: 5,
+      nombre: "Oscar",
+      apellido: "Padilla",
+      activo: false
+    },
+  ]
+
+  1. Explicar ciclo for normal
+  2. Explicar foreach
+  3. Explicar map
+  4. Explicar finder
+  5. Explicar find
+  6. Explicar como crear html con create element
+
+  const renderizar = () => {
+    const root = document.getElementById("root")
+    const h1 = document.createElement("h1")
+    h1.innerHTML="Soy un título"
+    root.appendChild(h1)
+    console.log(h1);
+    // root.innerHTML = card()
+    // console.log(root);
+  }
+*/
+
+
+
+
+
+
+const filter = ()=>{
+  return `
+    <div class="containter-input">
+      <input id="filter" type="text"/>
+    </div>
+  `
+}
+
+const card = (drake) => {
+  return `
+    <div class="card">
+      <h2 class="name">${drake.Nombre}</h2>
+      <img class="pic" src="./dragon.svg"/>
+      <div class="info">
+        <p class="type w-100"><span>Tipo:</span> ${drake.Tipo}</p>
+        <p class="w-100"><span>Description:</span> ${drake.Descripcion}</p>
+        <div class="container-attacks">
+          <h4 class="w-100">Ataques</h4>
+          ${
+            drake.Ataque.map((ataque)=>{
+              return `<p class="type w-49"><span>${ataque.Nombre}:</span> ${ataque.Daño}</p>`
+            }).join("")
+          }
+        </div>
+        <p class="type w-49"><span>Defensa:</span> ${drake.Defensa}</p>
+        <p class="type w-49"><span>Vida:</span> ${drake.Vida}</p>
+      </div>
+
+    </div>
+  `
+}
+
+const body = () => {
+  return `
+    ${filter()}
+    <div class="container" id="container">
+    </div>
+  `
+}
+const cards = (data) => {
+  return data.map((drake)=>{
+        return card(drake)
+      }).join("")
+}
+
+const renderCards = (data) => {
+  const container = document.getElementById("container")
+  container.innerHTML = cards(data)
+}
+
+const getFilter = () => {
+  const inputFilter = document.getElementById("filter")
+  let search = null;
+  inputFilter.addEventListener("keyup", (e)=>{
+    let valueInput = e.target.value
+     search = DataDragons.filter((drake)=>{
+      return (!valueInput ? drake : (drake.Nombre).toLowerCase().includes(valueInput.toLowerCase()))
+    })
+    renderCards(search)
+  })
+}
+
+
+
+const render = () => {
+  const root = document.getElementById("root")
+  root.innerHTML = body()
+  getFilter()
+  renderCards(DataDragons)
+}
+
+
+render()
